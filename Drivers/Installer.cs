0f0135a2b3fe77fba22158cce9f215fa6e0b9120
Dmitry <karyakin.dmitry@gmail.com>
2021-05-06 01:37:12 +0600

    first infrastructure attempts

---
+++ Drivers/Installer.cs
@@ -0,0 +1,21 @@
+﻿using SimpleInjector;
+
+namespace Drivers
+{
+  public static class Installer
+  {
+    private static Container container;
+
+    public static void Install()
+    {
+      if (container == null)
+      {
+        container = new Container();
+      }
+
+      container.Register(() => new WebDriverManager(), Lifestyle.Singleton);
+
+      container.Verify();
+    }
+  }
+}
---
+++ Drivers/WebDriverManager.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Drivers
+{
+  public class WebDriverManager
+  {
+    private IWebDriver _webDriver;
+
+    private void Initialize()
+    {
+      ChromeOptions options = new ChromeOptions
+      {
+        AcceptInsecureCertificates = true,
+        PageLoadStrategy = PageLoadStrategy.Eager,
+        UnhandledPromptBehavior = UnhandledPromptBehavior.Ignore
+      };
+
+      _webDriver = new ChromeDriver(options);
+
+      _webDriver.Manage().Window.Maximize();
+    }
+
+    public IWebDriver GetDriver()
+    {
+      if (_webDriver == null)
+      {
+        Initialize();
+      }
+
+      return _webDriver;
+    }
+  }
+}
